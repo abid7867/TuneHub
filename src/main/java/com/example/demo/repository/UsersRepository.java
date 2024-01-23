7ff8fc71803a3860dd6bbf55c8c57bae31cc9fa7
abid7867 <120393691+abid7867@users.noreply.github.com>
2024-01-23 17:55:13 +0530

    version 1.1--Tune hub

---
+++ src/main/java/com/example/demo/repository/UsersRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entities.Users;
+
+@Repository
+public interface UsersRepository
+					extends JpaRepository<Users, Integer>{
+
+	public Users findByEmail(String email);
+
+}
